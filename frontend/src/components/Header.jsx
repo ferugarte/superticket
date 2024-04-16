1f9c5dacd43a93e68059f4ac665ab9c89c33bb20
fernando.ugarte-BDEV <fernando.ugarte@bairesdev.com>
2024-04-16 18:15:23 -0400

    Unused imports removed in Header.jsx

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,12 +1,5 @@
 // src/components/Header.jsx
-import React, { useState } from 'react';
-import { AppBar, Toolbar, Typography, Icon, Drawer, List, ListItem, ListItemIcon, ListItemText, styled } from '@mui/material';
-import MenuIcon from '@mui/icons-material/Menu';
-import HomeIcon from '@mui/icons-material/Home';
-import EventIcon from '@mui/icons-material/Event';
-import CreateIcon from '@mui/icons-material/Create';
-import ListAltIcon from '@mui/icons-material/ListAlt';
-import VerifiedUserIcon from '@mui/icons-material/VerifiedUser';
+import { AppBar, Toolbar, Typography, styled } from '@mui/material';
 import { Link as RouterLink } from 'react-router-dom';
 
 const StyledToolbar = styled(Toolbar)({
@@ -14,16 +7,7 @@ const StyledToolbar = styled(Toolbar)({
 });
 
 const Header = () => {
-    const [isDrawerOpen, setIsDrawerOpen] = useState(false);
-
-    const toggleDrawer = (open) => (event) => {
-        if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
-            return;
-        }
-        setIsDrawerOpen(open);
-    };
-
-    return (
+      return (
         <>
             <AppBar position="static">
                 <StyledToolbar>
